1f3625115874cf18ac4e7c1386b800d595703024
Callum Dixon <callum.dixon@and.digital>
2022-10-05 11:52:49 +0100

    Initial Commit

---
+++ DynIsland/DynIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynIslandApp.swift
+//  DynIsland
+//
+//  Created by Callum Dixon on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DynIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
